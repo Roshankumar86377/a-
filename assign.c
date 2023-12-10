96478b97d24b96bd6d3b2649bd691becf1ed8d08
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-10 07:15:25 +0530

    Add files via upload

---
+++ assign.c
@@ -0,0 +1,11 @@
+/* WAP to add the given number by using the assignment operator */
+# include<stdio.h>
+int main(){
+    int a=4;
+    int b=6;
+    // a=a+b ko hm a+=b bhi lik sakte hai//
+    printf("sum is %d\n", a+=b);
+    // similarliy hum -,*,/ me bhi kr sakte hai //
+    return 0;
+    
+}
