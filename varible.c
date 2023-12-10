f02ee3360e91d0860605e805d706b5395c15bf35
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-10 07:11:35 +0530

    Add files via upload

---
+++ varible.c
@@ -0,0 +1,12 @@
+/* WAP to print add,subtract,multiply of given number */
+# include<stdio.h>
+int main(){
+    int a=12;
+    float b=3.43;
+    char c='R';
+    printf("Sum of a and b is %f\n", a+b);
+    printf("Subtract of a and b is %f\n", a-b);
+    printf("product of a and b is %f\n", a*b);
+
+    return 0;
+}
