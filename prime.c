e82d021bee5b26d3f6b27dfa89ce02b3630187d9
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-10 07:08:52 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,26 @@
+/* WAP to print the prime number between 1 to 100 */
+#include <stdio.h>
+int main()
+{
+    int i, a = 1, count;
+    while (a <= 100)
+    {
+        count = 0;
+        i = 2;
+        while (i <= a / 2)
+        {
+            if (a % i == 0)
+            {
+                count++;
+                break;
+            }
+            i++;
+        }
+        if (count == 0 && a != 1)
+        {
+            printf("%d \t", a);
+        }
+        a++;
+    }
+    return 0;
+}
