999b3c033a38ada4cde96de1d7a6c395b93fe45c
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-10 07:19:06 +0530

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,16 @@
+/* WAP to relational operator */
+# include<stdio.h>
+int main(){
+// true=1(2,3,4,-1,-3,etc)//
+// false=0//
+
+printf("%d\n", 3==3);
+printf("%d\n", 5==-5);
+printf("%d\n", 4>=2);
+printf("%d\n", 7>=9);
+printf("%d\n", 6<4);
+printf("%d\n", 3<9);
+printf("%d", 4!=4);
+
+return 0;
+}
