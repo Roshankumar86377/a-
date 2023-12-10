2f8fbd41940badaedcc62918aba246299522124b
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-10 07:19:46 +0530

    Add files via upload

---
+++ rem.c
@@ -0,0 +1,14 @@
+/* WAP to use the MODULUS operater */
+# include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter the value of a\n");
+    scanf("%d", &a);
+    printf("The remainder is %d\n", a%2);
+    printf("enter the value of b\n");
+    scanf("%d", &b);
+    printf("The remainder is %d\n", b%3);
+
+    return 0;
+    
+}
