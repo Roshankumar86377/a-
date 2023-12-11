104557e2026140d39b6d444b13bb32381be55f72
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-11 19:14:05 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,19 @@
+/* WAP to print the hello and bye by the use of recursion */
+# include<stdio.h>
+void printhello();
+void printbye();
+
+int main(){
+    printhello();
+    printbye();
+return 0;
+}
+
+void printhello() {
+printf("Hello Roshan\n");
+}
+
+void printbye() {
+    printf("Good Bye\n");
+}
+
