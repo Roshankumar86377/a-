5a0080b6b23b6141519d3d1e771460a8f0dfcf7f
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-11 19:07:11 +0530

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,10 @@
+/* WAP to print HELLO ROSHAN by the use of while loop */
+# include<stdio.h>
+int main(){
+    int i=1;
+    while(i<=5) {
+        printf("Helllo Roshan\n");
+        i++;
+    }
+    return 0;
+}
