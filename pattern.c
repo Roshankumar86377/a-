2c2f194416bc15f98ff2e94a625db2846cc3bc19
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-11 18:54:03 +0530

    Add files via upload

--- pattern.c
+++ pattern.c
@@ -1,16 +1,20 @@
-/* WAP to print a pattern e.x.=
-1
-23
-456
-78910 */
+/* Write a program to display a pattern e.x.=
+*
+**
+***
+****       */
 # include<stdio.h>
 int main(){
-    int i,j,k=0;
-    for(i=1; i<=4; i++){
-        for(j=1; j<=i; j++){
-            k=k+1;
-            printf("%d", k);
+    int n,i=1,j;
+    printf("Enter the n:\n");
+    scanf("%d", &n);
+    while(i<=n){
+        j=1;
+        while(j<=i){
+            printf("*");
+            j++;
         }
+        i++;
         printf("\n");
     }
     return 0;
