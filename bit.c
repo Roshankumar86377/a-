49498b05331bedae7d9d6383868350beae829543
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-11 19:13:00 +0530

    Add files via upload

---
+++ bit.c
@@ -0,0 +1,10 @@
+// In this page we write a program of bitwise of right shift and left shift// 
+# include<stdio.h>
+int main(){
+    int a=128,y=32;
+    a=a>>1;
+    printf("a is %d\n", a);
+    y=y<<2;
+    printf("y is %d\n", y);
+    return 0;
+}
