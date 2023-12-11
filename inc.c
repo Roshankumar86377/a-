04e05893e6875f877b0ad55d19806dee2812d6cc
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2023-12-11 19:14:34 +0530

    Add files via upload

---
+++ inc.c
@@ -0,0 +1,9 @@
+// In this program we code a increment and decrement value //
+# include<stdio.h>
+int main(){
+    int a=5,b;
+    b=a++;
+    printf("b is %d\n", b);
+    printf("a is %d\n", a);
+    return 0;
+}
