09b2d6c2c714374e8782648ba586a758bfd59494
Roshankumar86377 <153366199+Roshankumar86377@users.noreply.github.com>
2024-01-20 19:26:07 +0530

    Add files via upload

---
+++ string0.c
@@ -0,0 +1,10 @@
+/* WAP to print something by the use of string */
+
+# include<stdio.h>
+int main(){
+    char a[20];
+    printf("Enter the user name\n");
+    gets(a);
+    puts(a);
+    return 0;
+}
